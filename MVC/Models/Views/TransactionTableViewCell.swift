e68fdd50bf112d9421db487cc0ce84f0282ec512
West Kraemer <kraemerwest@gmail.com>
2019-02-05 15:06:02 -0500

    add iboutlets for labels in cell

---
+++ MVC/Models/Views/TransactionTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionTableViewCell.swift
+//  MVC
+//
+//  Created by West Kraemer on 2/5/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+    
+    
+}
