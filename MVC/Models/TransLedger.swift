4c931f3ad823a11608986bc1352f996bada75248
West Kraemer <kraemerwest@gmail.com>
2019-02-06 17:27:22 -0500

    add singleton for transaction ledger

--- MVC/Models/TransLedger.swift
+++ MVC/Models/TransLedger.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+//singleton
+class TransactionLedger {
+    
+    //global constant that can be used anywhere, but not created anywhere but here
+    static let sharedInstance = TransactionLedger()
+    
+    var transactions = [
+        Transaction(amount: 11.11, date: Date(), notes: "Banana"),
+        Transaction(amount: 22.22, date: Date(), notes: "Orange"),
+        Transaction(amount: 33.33, date: Date(), notes: "Grape"),
+        Transaction(amount: 44.44, date: Date(), notes: "Apple"),
+        Transaction(amount: 55.55, date: Date(), notes: "Mango"),
+        Transaction(amount: 66.66, date: Date(), notes: "Lemon"),
+        Transaction(amount: 77.77, date: Date(), notes: "Lime")
+    ]
+    
+    
+    private init() {
+        
+    }
+    
+}
