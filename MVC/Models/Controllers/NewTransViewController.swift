33998ccc307eb1f25a6f27282bff53b27141740c
West Kraemer <kraemerwest@gmail.com>
2019-02-06 17:59:17 -0500

    add and format text fields in new controller

---
+++ MVC/Models/Controllers/NewTransViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewTransViewController.swift
+//  MVC
+//
+//  Created by West Kraemer on 2/6/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class NewTransViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        
+    }
+    
+
+}
